55732141cf713ef32427f3179446194769cf3a4e
Fahad Karim <karimfahad@yahoo.com>
2018-05-29 21:19:13 -0400

    initial commit

---
+++ src/com/KeywordandExpressions/Main.java
@@ -0,0 +1,22 @@
+package com.KeywordandExpressions;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//false, true, null
+		//a miles = 1.609344
+		double kilometres = (100 * 1.609344); //except double keyword, rest of the sentence is expression
+		int highScore = 50; //everything except datatype and semi-colon
+		if (highScore == 50){   //everything in the bracket
+			System.out.println("this is an expression");
+		}
+	}
+}
+
+//lecture 24 quiz
+//score =100
+//score > 99
+//"you got high score"
+//score = 0;
+//Push again to remote
